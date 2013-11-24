ecea651740ef76c3808b6e861e4ebf57e348250b
Kyle Don Corbitt <kylecorbitt@gmail.com>
2013-11-23 23:12:50 -0700

    simple streaming to disk working

---
+++ pull_sample.py
@@ -0,0 +1,34 @@
+import json, time, datetime, os
+import tweepy
+
+secrets = json.loads(open("./secrets.json").read())
+
+auth = tweepy.OAuthHandler(secrets['consumer_key'], secrets['consumer_secret'])
+auth.set_access_token(secrets['access_token'], secrets['access_secret'])
+
+
+
+class RecordingListener(tweepy.streaming.StreamListener):
+    """Saves the tweet data to a log file"""
+
+    def on_connect(self):
+        self.log_file = open(os.path.join("raw","{0}.json".format(datetime.datetime.now()).replace(":",".").replace(" ","-")),'w')
+        self.log_file.write('[\n')
+
+    def on_data(self, data):
+        self.log_file.write('  '+data.strip()+",\n")
+        time.sleep(0.5)
+        return True
+
+    def on_error(self, status):
+        print status
+
+    def on_timeout(self):
+        print "Timeout at "+datetime.datetime.now()
+        sleep(60)
+
+print json.loads(open("raw\\2013-11-23-23.09.40.918000.json").read())
+
+# stream = tweepy.Stream(auth, RecordingListener())
+
+# stream.sample()
