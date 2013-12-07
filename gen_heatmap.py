008a0d9eb5998f55fba52fbf0e349c5aec76b930
Kyle Corbitt <kyle@corbt.com>
2013-12-07 15:35:56 -0700

    heatmap overlay created

---
+++ gen_heatmap.py
@@ -0,0 +1,18 @@
+import random, json
+import heatmap
+
+input_file = "2013-11-27-05.11.56.569021"
+
+hm = heatmap.Heatmap()
+points = []
+
+with open("raw/"+input_file+".json") as in_f:
+	for line in in_f:
+		tweet = json.loads(line)
+		coords = tweet['coordinates']['coordinates']
+		points.append(tuple(coords))
+
+
+img = hm.heatmap(points, dotsize=5, opacity=255, area=((-180,-90),(180,90)))
+# img.save("maps/"+input_file+".png")
+hm.saveKML("maps/"+input_file+".kml")
