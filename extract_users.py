900361b9be8f4e605741b22db9e81ea55f3eb505
Kyle Corbitt <kyle@corbt.com>
2013-12-06 17:33:10 -0700

    generates a subset of extracted tweets

---
+++ extract_users.py
@@ -0,0 +1,19 @@
+import os, json
+
+input_file = '24h_10k'
+
+# def get_country(tweet):
+
+with open('processed/'+input_file+'.json') as in_file:
+    data = json.loads(in_file.readline())
+    print json.dumps(data, indent=2)
+
+with open('processed/'+input_file+'.json') as in_file:
+    with open('processed/'+input_file+'_processed.json', 'w') as out_file:
+        for index,line in enumerate(in_file):
+            try:
+                tweet = json.loads(line)
+                out_file.write(json.dumps(tweet['user'])+'\n')
+            except Exception:
+                print "Tweet {0} failed to parse".format(index)
+
---
+++ get_subset.py
@@ -0,0 +1,26 @@
+# Gets N randomly-selected lines without replacement from the input file and saves them to the output file
+
+import os, random, json
+
+in_file = "raw/2013-11-27-05.11.56.569021.json"
+out_file = "processed/24h_10k.json"
+num_selected = 10000
+
+lines_with_location = []
+with open(in_file) as f:
+    for i,line in enumerate(f):
+        tweet = json.loads(line)
+        if tweet['user'] and tweet['user']['location'] and len(tweet['user']['location']) > 0:
+            lines_with_location.append(i)
+
+random.shuffle(lines_with_location)
+
+print "{0} entries with location discovered".format(len(lines_with_location))
+
+lines_selected = set(lines_with_location[0:num_selected])
+
+with open(in_file) as i_f:
+    with open(out_file, 'w') as o_f:
+        for i,line in enumerate(i_f):
+            if i in lines_selected:
+                o_f.write(line)
